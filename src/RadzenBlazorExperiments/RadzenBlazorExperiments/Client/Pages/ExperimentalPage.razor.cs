164d64ae71849a73fd2e1537b65ac78094fd4d1b
Michael Zyubin <MikhaZyu@gmail.com>
2022-10-21 12:44:57 +0300

    Test

---
+++ src/RadzenBlazorExperiments/RadzenBlazorExperiments/Client/Pages/ExperimentalPage.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RadzenBlazorExperiments.Client.Pages
+{
+    public partial class ExperimentalPage: ComponentBase
+    {
+        private void OnButtonClick()
+        {
+
+        }
+    }
+}
