75bd7df019d8b49fbc1443468d16948c2cc6032c
Michael Zyubin <MikhaZyu@gmail.com>
2023-01-19 21:48:41 +0300

    Experiments with HTML editor

---
+++ src/RadzenBlazorExperiments/RadzenBlazorExperiments/Client/Pages/EditorExperimentalPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace RadzenBlazorExperiments.Client.Pages
+{
+    public partial class EditorExperimentalPage: ComponentBase
+    {
+        private string _text;
+
+        void OnPaste(HtmlEditorPasteEventArgs args)
+        {
+            Console.WriteLine($"Paste: {args.Html}");
+        }
+
+        void OnChange(string html)
+        {
+            Console.WriteLine($"Change: {html}");
+        }
+
+        void OnExecute(HtmlEditorExecuteEventArgs args)
+        {
+            Console.WriteLine($"Execute: {args.CommandName}");
+        }
+    }
+}
--- src/RadzenBlazorExperiments/RadzenBlazorExperiments/Client/Program.cs
+++ src/RadzenBlazorExperiments/RadzenBlazorExperiments/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using RadzenBlazorExperiments.Client;
 
 namespace Company.WebApplication1
@@ -11,6 +12,11 @@ namespace Company.WebApplication1
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            
+            builder.Services.AddScoped<DialogService>();
+            builder.Services.AddScoped<NotificationService>();
+            builder.Services.AddScoped<TooltipService>();
+            builder.Services.AddScoped<ContextMenuService>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
